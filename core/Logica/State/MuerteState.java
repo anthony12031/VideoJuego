e3be89443869215e53a85f14f5626a026598cd44
Anthony Vargas <tony_jason@hotmail.com>
2015-05-24 14:43:55 -0500

    .

--- core/Logica/State/MuerteState.java
+++ core/Logica/State/MuerteState.java
@@ -1,5 +1,16 @@
 package State;
 
-public class MuerteState {
+import WW.Representacion.Producto.RepresentacionGrafica;
+
+public class MuerteState implements RepGraficaState {
+
+	@Override
+	public void actualizar(RepresentacionGrafica rep) {
+		/*
+		 * Encargada de liberar recursos y mostrar animacion de muerte del
+		 * entemagico
+		 */
+
+	}
 
 }
