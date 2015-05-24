a5a520b43e1a42696a5129b9ad8f90d51f5de7ba
Anthony Vargas <tony_jason@hotmail.com>
2015-05-24 14:57:30 -0500

    .

--- core/Logica/State/RepGraficaState.java
+++ core/Logica/State/RepGraficaState.java
@@ -4,7 +4,12 @@ import WW.Representacion.Producto.RepresentacionGrafica;
 
 public interface RepGraficaState {
 
+	/*
+	 * define la interface para los distintos estados en los que puede esta el
+	 * objeto la clase representacion grafica mantiene una referencia al estado
+	 * actual y pasa sus solicitudes de actualizacion al objeto estado_actual.
+	 */
+
 	public void actualizar(RepresentacionGrafica rep);
-	
-	
+
 }
