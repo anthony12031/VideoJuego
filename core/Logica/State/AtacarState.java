557733d4a1ea9bfd2a0f9609ffe08c188a047bb4
Anthony Vargas <tony_jason@hotmail.com>
2015-05-24 14:45:07 -0500

    .

--- core/Logica/State/AtacarState.java
+++ core/Logica/State/AtacarState.java
@@ -1,5 +1,13 @@
 package State;
 
-public class AtacarState {
+import WW.Representacion.Producto.RepresentacionGrafica;
+
+public class AtacarState implements RepGraficaState{
+
+	@Override
+	public void actualizar(RepresentacionGrafica rep) {
+		/* Encargado de mostrar animacion de ataque */
+		
+	}
 
 }
